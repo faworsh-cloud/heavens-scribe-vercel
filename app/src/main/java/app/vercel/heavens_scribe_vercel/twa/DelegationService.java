30c9a691cf99c41da0801e0efd8a8f50da771fb4
faworsh-cloud <faworsh@gmail.com>
2025-11-11 16:17:16 +0900

    디지털 자산 링크(assetlinks) 추가

---
+++ app/src/main/java/app/vercel/heavens_scribe_vercel/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.heavens_scribe_vercel.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
